b828bd288d27c462a7fd39c43edd665cd1440e95
zhengminghui2 <zhengminghui99@gmail.com>
2021-02-27 22:20:25 +0800

    添加cpu和内存占有率的echarts

---
+++ views/js/cpuPercent.js
@@ -0,0 +1,75 @@
+var cpuPercentDom = document.getElementById('cpuPercent');
+var cpuPercentChart = echarts.init(cpuPercentDom,'vintage');
+var cpuPercentOption;
+
+cpuPercentOption = {
+
+    series: [{
+        type: 'gauge',
+        title:{
+            show: true
+        },
+        progress: {
+            show: true,
+            width: 18
+        },
+        axisLine: {
+            lineStyle: {
+                width: 18
+            }
+        },
+        axisTick: {
+            show: false
+        },
+        splitLine: {
+            length: 15,
+            lineStyle: {
+                width: 2,
+                color: '#999'
+            }
+        },
+        axisLabel: {
+            distance: 25,
+            color: '#999',
+            fontSize: 20
+        },
+        anchor: {
+            show: true,
+            showAbove: true,
+            size: 25,
+            itemStyle: {
+                borderWidth: 10
+            }
+        },
+
+        detail: {
+            valueAnimation: true,
+            fontSize: 80,
+            offsetCenter: [0, '70%'],
+            formatter: '{value}%'
+
+        }
+    }]
+};
+cpuPercentChart.showLoading();
+cpuPercentChart.setOption(cpuPercentOption);
+
+// const host = window.location.host
+// const host = "127.0.0.1:8988"
+const cpuWS = new WebSocket("ws://"+host+"/ws/cpuPercentSingle")
+cpuWS.onmessage = function (evt) {
+    const received_msg = evt.data;
+
+    const json = JSON.parse(received_msg);
+    Math.ceil(json[0])
+    cpuPercentChart.hideLoading();
+    cpuPercentChart.setOption({
+        series: [
+            {
+                data: [
+                    {value:Math.ceil(json[0]),name: 'CPU使用率(%)'}
+                ]
+            }
+        ]
+    })
+};
---
+++ views/js/memPercent.js
@@ -0,0 +1,70 @@
+var memPercentDom = document.getElementById('memPercent');
+var memPercentChart = echarts.init(memPercentDom,'vintage');
+var memPercentOption;
+
+memPercentOption = {
+
+    series: [{
+        type: 'gauge',
+        title:{
+            show: true
+        },
+        progress: {
+            show: true,
+            width: 18
+        },
+        axisLine: {
+            lineStyle: {
+                width: 18
+            }
+        },
+        axisTick: {
+            show: false
+        },
+        splitLine: {
+            length: 15,
+            lineStyle: {
+                width: 2,
+                color: '#999'
+            }
+        },
+        axisLabel: {
+            distance: 25,
+            color: '#999',
+            fontSize: 20
+        },
+        anchor: {
+            show: true,
+            showAbove: true,
+            size: 25,
+            itemStyle: {
+                borderWidth: 10
+            }
+        },
+
+        detail: {
+            valueAnimation: true,
+            fontSize: 80,
+            offsetCenter: [0, '70%'],
+            formatter: '{value}%'
+
+        }
+    }]
+};
+memPercentChart.showLoading();
+memPercentChart.setOption(memPercentOption);
+
+const memWS = new WebSocket("ws://"+host+"/ws/memPercent")
+memWS.onmessage = function (evt) {
+    const received_msg = evt.data;
+    memPercentChart.hideLoading();
+    memPercentChart.setOption({
+        series: [
+            {
+                data: [
+                    {value:received_msg,name: '内存使用率(%)'}
+                ]
+            }
+        ]
+    })
+};
